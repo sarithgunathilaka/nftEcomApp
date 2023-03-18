c67e070aa39e9f3d0df015af6386b27dbfac8f0d
Sarith Gunathilake <sgunathilake@noon.com>
2023-03-18 17:24:03 +0530

    product UI

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
-const NFTCard = () => {
+import { COLORS, SIZES, SHADOWS, assets } from '../constants'
+import { CircleButton, TestButton } from './Button'
+import { SubInfo, EthPrice, NFTTitle } from '../components/SubInfo'
+
+const NFTCard = ({ item }) => {
+    const navigation = useNavigation();
     return (
-        <View>
-            <Text>NFTCard</Text>
+        <View style={{
+            backgroundColor: COLORS.white,
+            borderRadius: SIZES.font,
+            marginBottom: SIZES.extraLarge,
+            margin: SIZES.base,
+            ...SHADOWS.dark
+        }}>
+            <View style={{ width: "100%", height: 250 }}>
+                <Image
+                    source={item.image}
+                    resizeMode='cover'
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        borderTopLeftRadius: SIZES.font,
+                        borderTopRightRadius: SIZES.font,
+                    }}
+                />
+                <CircleButton imgUrl={assets.heart} right={10} top={10} />
+            </View>
+            <SubInfo />
+            <View style={{ width: "100%", padding: SIZES.font }} >
+                <NFTTitle
+                    title={item.name}
+                    subTitle={item.creator}
+                    titleSize={SIZES.large}
+                    subTitleSize={SIZES.small}
+
+                />
+                <View style={{
+                    marginTop: SIZES.font,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                    <EthPrice price={item.price} />
+                    <TestButton
+                        minWidth={120}
+                        fontSize={SIZES.font}
+                        handlePress={() => navigation.navigate("Details", { item })}
+                    />
+                </View>
+            </View>
         </View>
     )
 }
 
-export default NFTCard
+export default NFTCard 
